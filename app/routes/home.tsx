1cfd825f03218ae2a8b46f07df54d9373bfb5f67
coji <coji@techtalk.jp>
2025-05-16 15:59:51 +0900

    Remove unused meta function from home route for cleaner code

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,13 +2,6 @@ import { Suspense } from 'react'
 import StationSearch from '~/components/station-search'
 import type { Route } from './+types/home'
 
-export function meta() {
-  return [
-    { title: 'New React Router App' },
-    { name: 'description', content: 'Welcome to React Router!' },
-  ]
-}
-
 export function loader({ context }: Route.LoaderArgs) {
   return {}
 }
